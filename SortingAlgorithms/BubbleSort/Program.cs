dc868f902f6111757db208b27e3afdf1714324f9
Alper Silistre <alpersilistre@gmail.com>
2019-10-25 01:36:38 +0300

    Add Bubble Sort

---
+++ SortingAlgorithms/BubbleSort/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[]{5, 1, 4, 2, 8, 9};
+
+            ApplyBubbleSort(array);
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static int[] ApplyBubbleSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        Swap(array, j, j + 1);
+                    }
+                }
+            }
+
+            return array;
+        }
+
+        private static void Swap(int[] array, int i, int j)
+        {
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
