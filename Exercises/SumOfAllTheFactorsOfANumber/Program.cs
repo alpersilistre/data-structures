d715d363d3486ba0e4a3b882343f3a33009d9be7
Alper Silistre <alpersilistre@gmail.com>
2019-10-25 10:57:44 +0300

    Add Sum of all the factors of a number exercise

---
+++ Exercises/SumOfAllTheFactorsOfANumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SumOfAllTheFactorsOfANumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumOfAllFactors(30));
+        }
+
+        static int SumOfAllFactors(int number)
+        {
+            var result = 0;
+
+            for (int i = 2; i < Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    if (i == (number / i))
+                    {
+                        result += i;
+                    }
+                    else
+                    {
+                        result += (i + (number / i));
+                    }
+                }
+            }
+
+            return result + 1 + number;
+        }
+    }
+}
