dfa73968888e207ace44971a9a651ee087aeecdd
Alper Silistre <alpersilistre@gmail.com>
2019-10-28 01:41:20 +0300

    Add SingleLinkedList class with Insert Front and Last functionalities. Add ability to display LinkedList node values.

---
+++ Exercises/FindAPairWithMaxProductInArray/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FindAPairWithMaxProductInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[] {1, 4, 3, 6, -7};
+
+            MaxProduct(array);
+        }
+
+        static void MaxProduct(int[] array)
+        {
+            if (array.Length < 2)
+                Console.WriteLine("No pair exists");
+
+            int a = array[0];
+            int b = array[1];
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] * array[j] > a * b)
+                    {
+                        a = array[i];
+                        b = array[j];
+                    }
+                }
+            }
+
+            Console.WriteLine($"Max product pair is {a}, {b}");
+        }
+    }
+}
---
+++ ImplementLinkedList/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace ImplementLinkedList
+{
+    class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+
+        public void Display()
+        {
+            System.Console.WriteLine(data);
+
+            if (next != null)
+            {
+                next.Display();
+            }
+        }
+    }
+
+    class SingleLinkedList
+    {
+        public Node head = null;
+
+        public Node tail = null;
+
+        public void Display()
+        {
+            if (head == null)
+            {
+                System.Console.WriteLine("Single Linked List is empty.");
+            }
+            else
+            {   
+                head.Display();
+            }
+        }
+
+        public void InsertFront(int data)
+        {
+            if (head == null)
+            {
+                var newNode = new Node(data);
+
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                var newNode = new Node(data);
+
+                newNode.next = head;
+
+                head = newNode;
+            }
+        }
+
+        public void InsertLast(int data)
+        {
+            if (head == null)
+            {
+                var newNode = new Node(data);
+
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                var newNode = new Node(data);
+
+                tail.next = newNode;
+                tail = newNode;
+                
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var singleLinkedList = new SingleLinkedList();
+
+            singleLinkedList.InsertFront(4);
+            singleLinkedList.InsertLast(3);
+            singleLinkedList.InsertLast(7);
+            singleLinkedList.InsertFront(9);
+
+            singleLinkedList.Display();
+        }
+    }
+}
